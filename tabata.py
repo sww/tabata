93e13c1b8d39362c198578fbbf9fd879d3dd02fa
Sean Wang <sean@decrypted.org>
2022-10-07 17:05:14 -0700

    Set the default log level to INFO.

--- tabata.py
+++ tabata.py
@@ -65,6 +65,7 @@ if __name__ == "__main__":
 
     logging_kwargs: Dict[str, Any] = {
         "format": "%(asctime)s %(levelname)s %(message)s",
+        "level": logging.INFO,
     }
 
     if args.debug:
