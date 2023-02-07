c7f4a98bbc4432daa2af21cc7e0b7450f4e4b879
Sean Wang <sean@decrypted.org>
2023-02-06 17:35:26 -0800

    Renamed the "countdown" argument to "wait", and added a countdown to the wait.

--- tabata.py
+++ tabata.py
@@ -5,17 +5,14 @@ import enum
 import logging
 import sched
 import subprocess
-import time
 
 from typing import Any, Dict
-
-
-SECOND = 1
 
 
 class SessionType(enum.Enum):
     ACTIVITY = "ACTIVITY"
     REST = "REST"
+    WARM_UP = "WARM UP"
 
 
 def noop():
@@ -34,12 +31,13 @@ def interval(type: SessionType, length: int, notify: int = 5) -> None:
     # Notification begins after `notify_at` seconds.
     notify_at = length - notify
 
-    # Subtract some time because we don't want the "{type.value} ends in" output
-    # to overlap into the count down.
-    s.enter(notify_at - 1.5, 1, say, argument=(f"{type.value} ends in",))
+    if notify:
+        # Subtract some time because we don't want the "{type.value} ends in" output
+        # to overlap into the count down.
+        s.enter(notify_at - 1.5, 1, say, argument=(f"{type.value} ends in",))
 
-    for i in range(notify):
-        s.enter(notify_at + i, i, say, argument=(f"{notify - i}",))
+        for i in range(notify):
+            s.enter(notify_at + i, i, say, argument=(f"{notify - i}",))
 
     # So we're blocking for `length` seconds.
     s.enter(length, 1, noop)
@@ -67,7 +65,14 @@ def tabata_session(cycles=8) -> None:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--countdown", action="store", type=int, default=0)
+    parser.add_argument(
+        "-w",
+        "--wait",
+        action="store",
+        type=int,
+        default=0,
+        help="Wait n seconds before the start of the Tabata session."
+    )
     parser.add_argument("-d", "--debug", action="store_true", default=False)
     parser.add_argument("rounds", action="store", nargs="?", type=int, default=8)
 
@@ -83,8 +88,10 @@ if __name__ == "__main__":
 
     logging.basicConfig(**logging_kwargs)
 
-    if args.countdown > 0:
-        say(f"Session begins in {args.countdown} seconds")
-        time.sleep(args.countdown)
+    if args.wait > 0:
+        # It takes about 3 seconds for say "Begin Warm Up" to finish before the
+        # countdown starts.
+        notify = min(args.wait // 2, 5)
+        interval(SessionType.WARM_UP, args.wait, notify=notify)
 
     tabata_session(args.rounds)
